2462dd3b8327fcd1aae18f04125b906ca2e7e597
yaup0002 <yaup0002@gmail.com>
2020-03-31 16:38:19 +0530

    pdf added

--- app/src/main/java/com/nopalyer/navigationdrawer/humanities/humanitiescourses.java
+++ app/src/main/java/com/nopalyer/navigationdrawer/humanities/humanitiescourses.java
@@ -1,7 +1,10 @@
 package com.nopalyer.navigationdrawer.humanities;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,6 +13,7 @@ import com.nopalyer.navigationdrawer.R;
 
 public class humanitiescourses extends AppCompatActivity {
 
+    private Button b1 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,17 @@ public class humanitiescourses extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Programmes Offered");
         toolbar.setTitleTextColor(Color.WHITE);
+
+         b1 = (Button) findViewById(R.id.humanities_syllabus_btn);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(humanitiescourses.this,humanities_phd.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
