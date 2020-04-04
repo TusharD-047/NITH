8f7531f6c1f8d7bca34432f6d606742f9f1d51c7
SHREYANSH <shreyanshjain2674@gmail.com>
2020-04-04 15:09:05 +0530

    vvhvyuvyun

--- app/src/main/java/com/nopalyer/navigationdrawer/login.java
+++ app/src/main/java/com/nopalyer/navigationdrawer/login.java
@@ -83,7 +83,7 @@ public class login extends AppCompatActivity {
                         // finish();  }
                         // else
                         finish();
-                        startActivity(new Intent(login.this, Verification.class));
+                        startActivity(new Intent(login.this, Vern.class));
                         finish();
 
                     } else {
