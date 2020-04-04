f482c4d07cf499f9a2375877da6a2d40e17e6ff8
SHREYANSH <shreyanshjain2674@gmail.com>
2020-04-04 15:20:29 +0530

    vvhvyuvyun

--- app/src/main/java/com/nopalyer/navigationdrawer/Test1.java
+++ app/src/main/java/com/nopalyer/navigationdrawer/Test1.java
@@ -10,5 +10,7 @@ public class Test1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test1);
+
+        nu
     }
 }
