3a5331c0fb8800b455ad4dd0d95dce5c8c474462
yaup0002 <yaup0002@gmail.com>
2020-04-04 18:14:58 +0530

    kya hai be

--- app/src/main/java/com/nopalyer/navigationdrawer/StudentsPage.java
+++ app/src/main/java/com/nopalyer/navigationdrawer/StudentsPage.java
@@ -10,5 +10,6 @@ public class StudentsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_students_page);
+
     }
 }
