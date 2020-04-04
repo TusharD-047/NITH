fe9cb90c520fda26fdee716d865c4f6829c108ce
yaup0002 <yaup0002@gmail.com>
2020-04-04 19:55:23 +0530

    kya hai be

--- app/src/main/java/com/nopalyer/navigationdrawer/chemistry/chemistry.java
+++ app/src/main/java/com/nopalyer/navigationdrawer/chemistry/chemistry.java
@@ -14,9 +14,9 @@ import androidx.cardview.widget.CardView;
 import com.nopalyer.navigationdrawer.R;
 
 public class chemistry extends AppCompatActivity {
-    ViewFlipper slider;
+
     CardView about5,programmes5,vision5,labs5;
-    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +25,14 @@ public class chemistry extends AppCompatActivity {
         vision5 = findViewById(R.id.vision1);
         programmes5 = findViewById(R.id.programmes);
         about5 = findViewById(R.id.about);
-        slider = findViewById(R.id.slide);
         labs5 = findViewById(R.id.labs);
-        toolbar.setTitleTextColor(Color.WHITE);
 
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Chemistry Department");
+        toolbar.setTitleTextColor(Color.WHITE);
 
-        int[] images = {R.drawable.chemistry1, R.drawable.chemistry1};
-        for(int image:images)
-        {FlipperImages(image);
 
-        }
         about5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,16 +58,6 @@ public class chemistry extends AppCompatActivity {
             }
         });
     }
-    public void FlipperImages(int image){
-        ImageView imageview = new ImageView(this);
-        imageview.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        imageview.setBackgroundResource(image);
 
-        slider.addView(imageview);
-        slider.setFlipInterval(2500);
-        slider.setAutoStart(true);
-        slider.setInAnimation(this,android.R.anim.slide_in_left);
-        slider.setOutAnimation(this,android.R.anim.slide_out_right);
-    }
 
 }
