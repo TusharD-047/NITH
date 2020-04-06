b80da3ac85e3a00d76974fce8dcc5aca564917d0
TusharD-047 <56781715+TusharD-047@users.noreply.github.com>
2020-04-06 15:07:14 +0530

    Faculty Added

---
+++ app/src/main/java/com/nopalyer/navigationdrawer/Login/verification.java
@@ -0,0 +1,16 @@
+package com.nopalyer.navigationdrawer.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nopalyer.navigationdrawer.R;
+
+public class verification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+    }
+}
